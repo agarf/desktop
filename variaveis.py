914196c92cd90b08be68f9c207bcf22c08554e28
ronaldo fraga da silva <fotografofraga@gmail.com>
2024-09-14 14:55:13 -0300

    variaveis

---
+++ variaveis.py
@@ -0,0 +1,17 @@
+print("Um codigo sem variavel.")
+
+print("Eu tenho", 11, "cachorros")
+print("")
+print("Um codigo com variavel")
+num = 12
+print("Eu tenho", num, "cachorros")
+
+x, y = 4, 7
+
+soma = x + y
+print(soma)
+
+sub = x - y
+print(sub)
+
+multiplicacao = x * y
