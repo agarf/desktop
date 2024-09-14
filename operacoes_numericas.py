1dc7d79d098ce1423cda053bfd4fc4e2a6a95977
ronaldo fraga da silva <fotografofraga@gmail.com>
2024-09-14 16:16:18 -0300

    comecei operacao numerica 2

---
+++ operacoes_numericas.py
@@ -0,0 +1,19 @@
+'''
+Escreva um Programa que imprime dois numeros de sua escolha
+e que depois imprime a soma, a subtração, a multiplicação,
+a divisão normal e a divisão inteira do maior pelo menor,
+e o resto(coloque na mensagem a palavra resto ao inves do
+símbolo %)
+'''
+
+x = 15
+y = 10
+print(x)
+print(y)
+
+print(x + y)
+print(x - y)
+print(x * y)
+print(x / y)
+print(x // y)
+print("resto 10 = ", x%y)
